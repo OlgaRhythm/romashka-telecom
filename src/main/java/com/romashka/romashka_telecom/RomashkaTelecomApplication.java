d1f1b166efcefde1666bddf0fe04a6e405def7e9
OlgaRhythm <y.o.a@yandex.ru>
2025-04-07 18:47:57 +0300

    Инициализация проекта

---
+++ src/main/java/com/romashka/romashka_telecom/RomashkaTelecomApplication.java
@@ -0,0 +1,13 @@
+package com.romashka.romashka_telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkaTelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkaTelecomApplication.class, args);
+	}
+
+}
