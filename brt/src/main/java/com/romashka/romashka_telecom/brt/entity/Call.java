1e5ac61a0148a32eb18ed1a54487e9fe339f82fe
OlgaRhythm <y.o.a@yandex.ru>
2025-04-30 13:07:31 +0300

    Добавлен комментарий

---
+++ brt/src/main/java/com/romashka/romashka_telecom/brt/entity/Call.java
@@ -0,0 +1,49 @@
+package com.romashka.romashka_telecom.brt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "calls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Call {
+
+    /**
+     * Уникальный идентификатор записи.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "call_id")
+    private Long callId;
+
+    //TODO: добавить тип исходящий или входящий звонок
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "caller_id", nullable = false)
+    private Caller callerId;
+
+    @Column(name = "contact_number")
+    private String contactNumber;
+
+    /**
+     * Дата и время начала звонка в формате ISO 8601.
+     */
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    /**
+     * Дата и время окончания звонка в формате ISO 8601.
+     */
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+}
---
+++ brt/src/main/java/com/romashka/romashka_telecom/brt/entity/Caller.java
@@ -0,0 +1,53 @@
+package com.romashka.romashka_telecom.brt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "callers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Caller {
+
+    /**
+     * Уникальный идентификатор абонента.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "caller_id")
+    private Long callerId;
+
+    /**
+     * Внешний ключ на таблицу subscribers.
+     */
+    // TODO: нужен ли нам вся информация о абоненте?
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "subscriber_id", nullable = false)
+//    private Subscriber subscriberId;
+    @Column(name = "subscriber_id")
+    private Long subscriberId;
+
+    /**
+     * Телефонный номер абонента.
+     */
+    @Column(name = "number", nullable = false, length = 20)
+    private String number;
+
+    /**
+     * Идентификатор тарифного плана.
+     */
+    @Column(name = "rate_id", nullable = false)
+    private Long rateId;
+
+    /**
+     * Дата начала действия тарифа.
+     */
+    @Column(name = "rate_date", nullable = false)
+    private LocalDateTime rateDate;
+}
